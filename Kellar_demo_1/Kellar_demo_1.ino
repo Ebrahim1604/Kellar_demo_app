6686bb236a158247fa8f66867c3740b0bdd86a12
Ebrahim1604 <42499258+Ebrahim1604@users.noreply.github.com>
2021-08-02 02:39:16 +0530

    Update Kellar_demo_1.ino

--- Kellar_demo_1/Kellar_demo_1.ino
+++ Kellar_demo_1/Kellar_demo_1.ino
@@ -9,6 +9,8 @@
     5)    GND-----------------------------GND    
  */
 
+#include <stdint.h>
+
 // Include the header for the ModbusClient RTU style
 #include "ModbusClientRTU.h"
 
@@ -16,60 +18,27 @@
 // For demonstration, use the LOG statements for output
 #include "Logging.h"
 
-
-
 const byte rxd = 12;
 const byte txd = 13;
 const byte rts = 15;
 
-#define demo_addr1 0x0200 //baud rate confg
+#define no_of_sensors 1
 
-#define demo_addr2 0x0202 // S/n h bytes
-
-#define demo_addr3 0x0203 // S/n l bytes
+uint16_t addr_list[] = {0x0002,0x0004,0x0006,0x0008,0x000A}; //p1,p2,T,tob1,tob2
 
 // Create a ModbusRTU client instance
 ModbusClientRTU MB(Serial2, rts);
-
-// Define an onData handler function to receive the regular responses
-// Arguments are Modbus server ID, the function code requested, the message data and length of it, 
-// plus a user-supplied token to identify the causing request
-void handleData(ModbusMessage response, uint32_t token) 
-{
-  Serial.print("handledata() function running on core: ");
-  Serial.println(xPortGetCoreID());
-  Serial.println("");
-  
-  // Only print out result of the "real" example - not the request preparing the field
-  if (token > 1111) {
-    LOG_N("Response: serverID=%d, FC=%d, Token=%08X, length=%d:\n", response.getServerID(), response.getFunctionCode(), token, response.size());
-    HEXDUMP_N("Data", response.data(), response.size());
-  }
-}
-
-// Define an onError handler function to receive error responses
-// Arguments are the error code returned and a user-supplied token to identify the causing request
-void handleError(Error error, uint32_t token) 
-{
-  Serial.print("handle error() function running on core: ");
-  Serial.println(xPortGetCoreID());
-  Serial.println("");
-  // ModbusError wraps the error code and provides a readable error message for it
-  ModbusError me(error);
-  LOG_E("Error response: %02X - %s\n", (int)me, (const char *)me);
-}
 
 // Setup() - initialization happens here
 void setup() {
 // Init Serial monitor
   Serial.begin(115200);
   while (!Serial) {}
+  
   Serial.println("__ Kellar33x Modbus demo code __");
   Serial.println("");
   Serial.println("");
-  Serial.print("Setup() function running on core: ");
-  Serial.println(xPortGetCoreID());
-  Serial.println("");
+
 // Set up Serial2 connected to Modbus RTU
 // (Fill in your data here!)
   Serial2.begin(9600, SERIAL_8N1, rxd, txd);
@@ -84,71 +53,84 @@ void setup() {
 // Start ModbusRTU background task
   MB.begin();
 
-  uint32_t Token = 1111;
+uint32_t Token = 1111;
 
-// Create request by
-// - token to match the response with the request.
-// - server ID = 1
-// - function code = 0x03 (read holding register)
-// - address to read = word 10
-// - data words to read = 1
-//
-// If something is missing or wrong with the call parameters, we will immediately get an error code 
-// and the request will not be issued
-Serial.println("_______________________________________________________________________");
-Serial.println(" 1) Reading the reg addr 0x0200, confg register");
-Serial.println("");
-// Read register demo_addr1
- Error err = MB.addRequest(Token++, 1, READ_HOLD_REGISTER, demo_addr1, 1);
-  if (err!=SUCCESS) {
-    ModbusError e(err);
-    LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
-  }
+for(int id = 1; id<=no_of_sensors; id++)
+  {   
+      Serial.printf("Parameters for Sensor %d:\n", id);
+      
+      for(int addr = 0; addr<5; addr++)
+      {
+        Error err = MB.addRequest(Token++, id, READ_HOLD_REGISTER, addr_list[addr], 2);
+          if (err!=SUCCESS) 
+          {
+          ModbusError e(err);
+          LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
+          }
+       }
+    }
 
-Serial.println("_______________________________________________________________________");
-Serial.println(" 2) Reading the reg addr 0x0202, H byte of serial number: 728862, H bytes: 00 0B");
-Serial.println("");
-  // Read register demo_addr2
-  err = MB.addRequest(Token++, 1, READ_HOLD_REGISTER, demo_addr2, 1);
-  if (err!=SUCCESS) {
-    ModbusError e(err);
-    LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
-  }
-
-Serial.println("_______________________________________________________________________");
-Serial.println(" 3) Reading the reg addr 0x0203, L byte of serial number: 728862, L bytes: 1F 1E");
-Serial.println("");
-  // Read register demo_addr3
-  err = MB.addRequest(Token++, 1, READ_HOLD_REGISTER, demo_addr3, 1);
-  if (err!=SUCCESS) {
-    ModbusError e(err);
-    LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
-  }
-  
-/*
-// Now write 0xBEEF to it
-  err = MB.addRequest(Token++, 1, WRITE_HOLD_REGISTER, 10, 0xBEEF);
-  if (err!=SUCCESS) {
-    ModbusError e(err);
-    LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
-  }
-
-// Read it again to verify
-  err = MB.addRequest(Token++, 1, READ_HOLD_REGISTER, 10, 1);
-  if (err!=SUCCESS) {
-    ModbusError e(err);
-    LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
-  }
-
-// Provoke an error just to show it
-  err = MB.addRequest(Token++, 1, USER_DEFINED_44, 10, 1);
-  if (err!=SUCCESS) {
-    ModbusError e(err);
-    LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
-  }
-*/
 }
+
 // loop() - nothing done here today!
 void loop() 
 {
+  /*
+  for(int id = 1; id<=no_of_sensors; id++)
+  {   
+      Serial.printf("Parameters for Sensor %d:\n", id);
+      
+      for(int addr = 0; addr<5; addr++)
+      {
+        Error err = MB.addRequest(Token++, id, READ_HOLD_REGISTER, addr_list[addr], 2);
+          if (err!=SUCCESS) 
+          {
+          ModbusError e(err);
+          LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
+          }
+       }
+    }
+    */
 }
+
+void handleData(ModbusMessage response, uint32_t token) 
+{
+  // Only print out result of the "real" example - not the request preparing the field
+  if (token > 1111) {
+
+    const uint8_t *val = response.data();
+    int val_size = response.size();
+    //static uint8_t val[val_size];
+    
+    Serial.print("Data received = ");
+    for(int k=0; k<val_size; k++)
+    {
+      
+      Serial.print(val[k]);
+      Serial.print(" ");
+    }
+    Serial.println("");
+  }
+}
+
+void handleError(Error error, uint32_t token) 
+{
+  // ModbusError wraps the error code and provides a readable error message for it
+  ModbusError me(error);
+  LOG_E("Error response: %02X - %s\n", (int)me, (const char *)me);
+}
+
+/*
+  union
+    {
+        float doubleVal;
+        uint8_t bytes[4];
+    }doubleConverter;
+       
+    doubleConverter.bytes[0]= 0x7B; 
+    doubleConverter.bytes[1]= 0xF0;
+    doubleConverter.bytes[2]= 0x75;
+    doubleConverter.bytes[3]= 0x3F;
+  
+    double fo = (double)doubleConverter.doubleVal;
+ */
