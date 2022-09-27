df2a12c60250bfa65529697c77a26e20aab0d05d
Ebrahim1604 <42499258+Ebrahim1604@users.noreply.github.com>
2022-09-28 02:37:48 +0530

    Create Kellarbus-master-Esp32-multiple.ino

---
+++ Kellarbus-master-Esp32-multiple/Kellarbus-master-Esp32-multiple.ino
@@ -0,0 +1,96 @@
+/**
+
+  @author thewknd
+  @date 7.2012
+
+  @copyright
+  This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+*/
+
+#include "kellerbus.h"
+#include "time/TimeLib.h"
+#include "SoftwareSerial.h"
+
+#define NOD 8 //number of devices connected to the kellerbus
+
+/*
+  RX Pin: 12
+  TX Pin: 13
+*/
+
+// digital pins 12 and 13 are used as virtual RX and TX serial lines.
+#define rxPin 12
+#define txPin 13
+
+SoftwareSerial mySerial;
+
+CKellerBus kbus((Stream*)&mySerial, 9600, 2, 100);
+
+time_t kbusTime;
+
+uint8_t mclass, group, myear, week, cfg_p, cfg_t, cnt_t, buffer, state;
+int serial;
+double temp, pressure;
+    
+  unsigned long time1,time2;
+
+void setup()
+{
+  //define pinmodes for rx & tx
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+
+  Serial.begin(115200);
+
+  mySerial.begin(9600, SWSERIAL_8N1, rxPin, txPin, false);
+  if (!mySerial)
+  {
+    Serial.println("SoftSerial configuration not done correctly");
+    while (1)
+    {
+      delay(1000);
+    }
+  }
+}
+
+void loop()
+{
+  time1 = 0;
+  time2 = 0;
+  
+  time1 = millis();
+  
+  for(int i=1; i<=NOD; i++)
+  {
+    temp = 0;
+    pressure = 0;
+    
+    Serial.println("\n-----------------");
+    
+    kbus.initDevice(i, &mclass, &group, &myear, &week, &buffer, &state);
+   
+    Serial.print("DEVICE ADDRESS : ");
+    Serial.println(kbus.getDeviceAddress(), DEC);
+    Serial.print("SN : ");
+    serial = kbus.getSerialnumber();
+    Serial.println(serial);
+    
+    Serial.println("\n\r- Measurement -");
+    
+    Serial.println(kbus.getCH0(), DEC);
+    Serial.print("P1 : ");
+    pressure = kbus.getP1(P_BAR);
+    Serial.println(pressure);
+    Serial.print("TOB1 : ");
+    temp = kbus.getTOB1(T_DEGC);
+    Serial.println(temp);
+  }
+  
+  time2 = millis();
+
+  Serial.println("\n-------------------------------------------------------");
+  
+  Serial.printf("Time taken for reading %d sensors = ",NOD);
+  Serial.print((time2-time1)/1000.0);
+  Serial.println(" secs");
+}
